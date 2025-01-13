e3d1c9ecc3009052b086d9f6f6ae381bfbb27bd5
almasayana <ayana.takoch@gmail.com>
2025-01-13 22:01:22 +0500

    Comments examples

---
+++ tsis1/comments.py
@@ -0,0 +1,24 @@
+#1st example
+#This is a comment
+print("Hello, World!")
+
+#2nd example
+print("Hello, World!") #This is a comment
+
+#3rd example
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#4th example
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#5th example
+"""
+This is a comment
+written in
+more than just one line 
+"""
+print("Hello, World!")
