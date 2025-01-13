64ef133057a2ff1c25f53ac577fe75769b099ae4
almasayana <ayana.takoch@gmail.com>
2025-01-13 22:52:10 +0500

    Variables examples

---
+++ tsis1/variables/var_name.py
@@ -0,0 +1,12 @@
+#1
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#2
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
