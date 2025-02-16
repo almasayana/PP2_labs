64665435d3d46ea6e9d2dd6262c03f8af8c57f0f
almasayana <ayana.takoch@gmail.com>
2025-02-16 22:35:58 +0500

    upd

---
+++ tsis4/generators.py
@@ -0,0 +1,49 @@
+#1
+def sq_generator(n):
+    for i in range(n + 1):
+        yield i ** 2
+
+print(f"The squares of numbers:")
+for num in sq_generator(4):
+    print(num, end=" ")
+print('\n')
+
+#2
+def even_generator(n):
+    for i in range(0, n + 1, 2):
+        yield i
+
+n = int(input("Enter a number: "))
+print(f"Even numbers between 0 and {n}:")
+print(", ".join(map(str, even_generator(n))))
+print()
+
+#3
+def div_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
+n = int(input("Enter a number: "))
+print(f"Numbers which are divisible by 3 and 4, between a given range 0 and {n}:")
+print(list(div_by_3_and_4(n)))
+print()
+
+#4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i ** 2
+
+print("the square of all numbers between a given range:")
+for num in squares(7, 15):
+    print(num, end=" ")
+print('\n')
+
+#5
+def rev(n):
+    for i in range(n, -1, -1):
+        yield i
+
+print('Numbers from a given N down to 0:')
+for num in rev(6):
+    print(num, end=" ")
--- tsis4/math_library.py
+++ tsis4/math_library.py
@@ -1,14 +1,14 @@
 import math
 #1
 degree = int(input("Input degree: "))
-print(f"It is {degree * math.pi / 180:.6f} in radian")
+print(f"Output radian: {degree * math.pi / 180:.6f}")
 print()
 
 #2
 h = int(input("Height: "))
 a = int(input("Base, first value: "))
 b = int(input("Base, second value: "))
-print("The are of a trapezoid is:", (a+b)*h/2)
+print("Expected Output:", (a+b)*h/2)
 print()
 
 #3
@@ -16,10 +16,10 @@ n = int(input("Input number of sides: "))
 s = float(input("Input the length of a side: "))
 
 area = (n * s**2) / (4 * math.tan(math.pi / n))
-print(int(area))
+print(f"The area of the polygon is: {area:.1f}")
 print()
 
 #4
 a = int(input('Length of base: '))
 b = int(input('Height of parallelogram: '))
-print(f"The area of a parallelogram{a*b:.1f}")
+print(f"Expected Output: {a*b:.1f}")
